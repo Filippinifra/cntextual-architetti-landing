325eb0b4e7cd731377f12b4cfb684e2f8834d671
Filippinifra <filippinifra123@gmail.com>
2024-02-05 20:12:52 +0100

    add cursor

---
+++ src/components/style.css.ts
@@ -0,0 +1,32 @@
+import { style } from "@vanilla-extract/css";
+
+export const cursorClass = style({
+  position: "fixed",
+  borderRadius: "50%",
+  mixBlendMode: "screen",
+  pointerEvents: "none",
+  transform: "translate(-50%, -50%)",
+  zIndex: "999999 !important",
+  backdropFilter: "blur(1px)",
+  transition: "width 0.2s ease-in-out, height 0.2s ease-in-out",
+  border: "1px solid lightgrey",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "#3a3b3c",
+
+  "@media": {
+    "screen and (max-width: 768px)": {
+      width: 0,
+      height: 0,
+      transition:
+        "width 0.2s ease-in-out, height 0.2s ease-in-out, opacity 0.2s ease-in-out",
+    },
+  },
+});
+
+export const pointerClass = style({
+  opacity: "0 !important",
+  transition:
+    "width 0.2s ease-in-out, height 0.2s ease-in-out, opacity 0.2s ease-in-out",
+});
