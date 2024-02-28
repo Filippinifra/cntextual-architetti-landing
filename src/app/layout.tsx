4f2a67f2028b0977e7af78b64f105bb2853ec747
Filippinifra <filippinifra123@gmail.com>
2024-02-28 19:01:30 +0100

    add seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,8 +16,37 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="it">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/seo/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/seo/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/seo/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/seo/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/seo/safari-pinned-tab.svg"
+          color="#ffffff"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <html lang="it">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   );
 }
